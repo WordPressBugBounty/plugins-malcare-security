aac08f2dc09dff9eb58cf79954dc29da88e269b5
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 03:29:08 +0000

    6.36

--- malcare-security/protect/lib/utils.php
+++ malcare-security/protect/lib/utils.php
@@ -1,8 +1,8 @@
 <?php
 if (!defined('ABSPATH') && !defined('MCDATAPATH')) exit;
 
-if (!class_exists('MCProtectUtils_V602')) :
-class MCProtectUtils_V602 {
+if (!class_exists('MCProtectUtils_V636')) :
+class MCProtectUtils_V636 {
 	public static function getIP($ip_header) {
 		$ip = null;
 		if (is_array($ip_header)) {
@@ -130,7 +130,7 @@ class MCProtectUtils_V602 {
 
 		if (is_array($val)) {
 			foreach ($val as $e) {
-				$length += MCProtectUtils_V602::getLength($e);
+				$length += MCProtectUtils_V636::getLength($e);
 			}
 
 			return $length;
--- malcare-security/protect/logger.php
+++ malcare-security/protect/logger.php
@@ -1,21 +1,21 @@
 <?php
 if (!defined('ABSPATH') && !defined('MCDATAPATH')) exit;
 
-if (!class_exists('MCProtectLogger_V602')) :
+if (!class_exists('MCProtectLogger_V636')) :
 require_once dirname( __FILE__ ) . '/logger/fs.php';
 require_once dirname( __FILE__ ) . '/logger/db.php';
 
-class MCProtectLogger_V602 {
+class MCProtectLogger_V636 {
 	private $log_destination;
 
 	const TYPE_FS = 0;
 	const TYPE_DB = 1;
 
-	function __construct($name, $type = MCProtectLogger_V602::TYPE_DB) {
-		if ($type == MCProtectLogger_V602::TYPE_FS) {
-			$this->log_destination = new MCProtectLoggerFS_V602($name);
+	function __construct($name, $type = MCProtectLogger_V636::TYPE_DB) {
+		if ($type == MCProtectLogger_V636::TYPE_FS) {
+			$this->log_destination = new MCProtectLoggerFS_V636($name);
 		} else {
-			$this->log_destination = new MCProtectLoggerDB_V602($name);
+			$this->log_destination = new MCProtectLoggerDB_V636($name);
 		}
 	}
 
